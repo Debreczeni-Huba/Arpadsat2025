10ea22ae41909477388f2ef63e5fee9636bcaab1
Debreczeni-Huba <huba.debreczeni@gmail.com>
2024-11-23 23:18:45 +0100

    202411232318

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import htmlContent from './index.html';
+import htmlContent from '/index.html';
 
 const Component: React.FC = () => {
   return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
